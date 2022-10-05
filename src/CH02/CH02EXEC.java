9afacc6d6e33456f9a8884627506f46652a0ae45
syrah97 <dcyc8326@naver.com>
2022-10-05 15:50:28 +0900

    SYRAH 1.0

---
+++ src/CH02/CH02EXEC.java
@@ -0,0 +1,48 @@
+package CH02;
+
+public class CH02EXEC {
+
+	public static void main(String[] args) {
+		
+		
+		
+	//		정수 타입 		: byte,short,int ,long
+	//		char 타입 	: ' ' 
+	//		String 타입 	: " "
+	//		실수 타입 		: double , float
+	//		논리 타입 		: boolean
+	//
+	//
+	//		확인 문제1
+	//
+	//		정수타입 
+	//		1byte : byte
+	//		2byte : short
+	//		4byte : int
+	//		8byte : long
+	//
+	//		실수타입
+	//		4byte : float
+	//		8byte : double
+	//
+	//		논리타입
+	//		1byte : boolean
+	//
+	//
+	//		확인문제2
+	//
+	//		맞는 코드인지 틀린 코드인지 확인 
+	//		byte var = 200;							( x	)
+	//		char var='AB';							( x	)
+	//		char var=65;							( o	)
+	//		long var=50000000000;					( x	)	
+	//		float var = 3.14						( x	)
+	//		double var = 100.0						( o	)
+	//		String var = "나의직업은 "개발자" 입니다.";		( x	)
+	//		boolean var = 0;						( x	)
+	//		int v2 = le2;							( x	)
+	//		float =1e2f;							( x	)
+
+	}
+
+}
