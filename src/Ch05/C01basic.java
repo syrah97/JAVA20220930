a844066f9355307d0b8a33b1051382fee397ffec
syrah97 <dcyc8326@naver.com>
2022-10-07 12:25:21 +0900

    SYRAH 1.0

--- src/Ch05/C01basic.java
+++ src/Ch05/C01basic.java
@@ -3,23 +3,41 @@ package Ch05;
 public class C01basic {
 	public static void main(String[] args) {
 ////	기본 산술 연산자
-		int a = 10, b = 20, c;
-		System.out.println("a+b="+ (a + b));
-		System.out.println("a-b="+ (a - b));
-		System.out.println("a*b="+ (a * b));
-		System.out.println("b/a="+ (b / a)); //나누기 - 몫을 구하는 작업		
-		System.out.println("a%%b="+ (a % b));//나누기 - 나머지를 구하는 작업(1.짝홀수구분,2.배수구분,3.자리수제한,4.끝자리구하기)
-		System.out.println("-a="+ -a);
+//		int a = 10, b = 20, c;
+//		System.out.println("a+b="+ (a + b));
+//		System.out.println("a-b="+ (a - b));
+//		System.out.println("a*b="+ (a * b));
+//		System.out.println("b/a="+ (b / a)); //나누기 - 몫을 구하는 작업		
+//		System.out.println("a%%b="+ (a % b));//나누기 - 나머지를 구하는 작업(1.짝홀수구분,2.배수구분,3.자리수제한,4.끝자리구하기)
+//		System.out.println("-a="+ -a);
 
 		//문제	
 //		두수를 입력 받아 두수의 합/차/곱을 출력하는 프로그램을 만들어보세요
 //		Scanner 를 사용합니다
 		
+		
+		//대입연산자
+		//LV = RV;
+		//공간 = 값(우선 처리);
+		
 	
-		////복합대입 연산자
+		////복합대입 연산자(산술+대입)
 //		int a = 10;
-//		a += 10;
+//		a += 10; //a = a + 10;
+//		a -= 5; // a = a-5;
+//		a *= 3; // a = a*3;
 //		System.out.println("a="+a);
+		
+		
+		////비교연산자
+		int a =10;
+		int b=20;
+		System.out.println("A == B : "+ (a == b));
+		System.out.println("A  > B : "+ (a > b));
+		System.out.println("A  < B : "+ (a < b));
+		System.out.println("A >= B : "+ (a >= b));
+		System.out.println("A <= B : "+ (a <= b));
+		System.out.println("A != B : "+ (a != b));
 
 		
 		////논리연산자 
@@ -40,16 +58,6 @@ public class C01basic {
 //		play = !play;
 //		System.out.println(play);
 		
-
-		////비교연산자
-//		int a =10;
-//		int b=20;
-//		System.out.println("A == B : "+ (a == b));
-//		System.out.println("A  > B : "+ (a > b));
-//		System.out.println("A  < B : "+ (a < b));
-//		System.out.println("A >= B : "+ (a >= b));
-//		System.out.println("A <= B : "+ (a <= b));
-//		System.out.println("A != B : "+ (a != b));
 
 
 	
