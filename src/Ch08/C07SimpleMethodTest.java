2dde30198b890f338d55dedc9ad010a70df8b431
syrah97 <dcyc8326@naver.com>
2022-10-13 12:43:49 +0900

    SYRAH 1.0

--- src/Ch08/C07SimpleMethodTest.java
+++ src/Ch08/C07SimpleMethodTest.java
@@ -10,6 +10,22 @@ class Sum{
 	int sum1(int x,int y) {
 		return x+y;
 	}
+	//반환 o 매개변수 x
+	int sum2() {
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		return x+y;
+	}
+	//반환 x 매개변수 o
+	void sum3(int x, int y) {
+		System.out.println("sum3(x,y) = " + (x+y));
+	}
+	//반환 x 매개변수 o
+	void sum4() {
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		System.out.println("sum4() = " + (x+y));
+	}
 }
 
 public class C07SimpleMethodTest {
@@ -18,9 +34,16 @@ public class C07SimpleMethodTest {
 
 			Sum calc = new Sum();	//Sum객체 생성 이후 calc 참조변수에 연결
 			
-			int r1 = calc.sum1(10,20);	//calc객체의 sum1함수를 실행
-			System.out.println("sum1(x,y) = " + r1);
+//			int r1 = calc.sum1(10,20);	//calc객체의 sum1함수를 실행
+//			System.out.println("sum1(x,y) = " + r1);
+//			
+//			int r2 = calc.sum2();
+//			System.out.println("sum2() = " + r2);
 
+			calc.sum3(12, 34);
+			
+			calc.sum4();
+			System.out.println("프로그램을 종료합니다");
 	}
 
 }
