55b0a1ee00c00ba1bafb9a2130e4fcf557636645
syrah97 <dcyc8326@naver.com>
2022-10-13 12:24:04 +0900

    SYRAH 1.0

---
+++ src/Ch08/C07SimpleMethodTest.java
@@ -0,0 +1,26 @@
+package Ch08;
+
+import java.util.Scanner;
+
+class Sum{
+	//속성
+	Scanner sc = new Scanner(System.in);	
+	//기능
+	//반환 o 매개변수 o
+	int sum1(int x,int y) {
+		return x+y;
+	}
+}
+
+public class C07SimpleMethodTest {
+
+	public static void main(String[] args) {
+
+			Sum calc = new Sum();	//Sum객체 생성 이후 calc 참조변수에 연결
+			
+			int r1 = calc.sum1(10,20);
+			System.out.println("sum1(x,y) = " + r1);
+
+	}
+
+}
