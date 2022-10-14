1d081012e687428c85e27b067657e28a511234f1
syrah97 <dcyc8326@naver.com>
2022-10-14 12:28:58 +0900

    SYRAH 1.0

--- src/CH09/C02This.java
+++ src/CH09/C02This.java
@@ -4,9 +4,33 @@ package CH09;
 // 클래스 내에서 사용되는 예약어
 // 생성되는 객체의 위치정보를 가져오는데 사용
 
+// 멤버변수 vs 매개변수 구별
+// this.name = name
+
+// 오버로딩된 다른 생성자 호출 가능
 
 class C02Simple{
 	int x;
+	int y;
+	
+	C02Simple(){
+		x=10;y=10;
+//		this(10,10);	//C02Simple(int x, int y)생성자 호출
+		System.out.println("디폴트 생성자 호출!");
+	}
+	C02Simple(int x){
+		this.x=x;y=10;
+//		this(x,10);		//C02Simple(int x, int y)생성자 호출	
+		System.out.println("int x 생성자호출!");
+	}
+	C02Simple(int x, int y){
+		this.x=x;this.y=y;
+		System.out.println("int x,int y 생성자 호출!");
+	}
+	
+	void setX(int x) {
+		this.x = x;
+	}
 	
 	C02Simple getThis() {
 		return this;
@@ -16,9 +40,13 @@ class C02Simple{
 public class C02This {
 
 	public static void main(String[] args) {
-		C02Simple ob = new C02Simple();
-		System.out.println(ob);
-		System.out.println(ob.getThis());
+//		C02Simple ob = new C02Simple();
+//		System.out.println(ob);
+//		System.out.println(ob.getThis());
+		
+		C02Simple ob1 = new C02Simple();
+//		C02Simple ob2 = new C02Simple(100);
+//		C02Simple ob3 = new C02Simple(100,200);
 
 	}
 
---
+++ src/CH09/C03StringClass.java
@@ -0,0 +1,10 @@
+package CH09;
+
+public class C03StringClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
