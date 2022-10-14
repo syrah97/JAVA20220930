6579f729a54b0549cfcffb8f6c1e67a82933e9e6
syrah97 <dcyc8326@naver.com>
2022-10-14 12:32:22 +0900

    SYRAH 1.0

--- src/CH09/C03StringClass.java
+++ src/CH09/C03StringClass.java
@@ -3,7 +3,17 @@ package CH09;
 public class C03StringClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String str1 = "java";
+		String str2 = "java";
+		
+		String str3 = new String("java");
+		String str4 = new String("java");
+		
+		System.out.println("str1 = str2 : "+(str1==str2));
+		System.out.println("str3 = str4 : "+(str3==str4));
+		System.out.println("str1 = str3 : "+(str1==str3));
+		System.out.println("str1 = str4 : "+(str1==str4));
+		//equals 메서드 사용하여 문자열 자체 비교하기
 
 	}
 
