679d9dd8aeff50d74e38be07b698c7dfaef340d7
syrah97 <dcyc8326@naver.com>
2022-10-17 12:27:57 +0900

    SYRAH 1.0

--- src/CH10/CH03StoreMain.java
+++ src/CH10/CH03StoreMain.java
@@ -7,8 +7,11 @@ class Buyer{
 	//기능
 	//구매하기
 	void Pay(Seller seller,int money){
-		myMoney=money;
+		//보유금액에 금액 차감
+		myMoney-=money;
+		//seller에 money 전달 사과 개수 리턴받음
 		int cnt=seller.returnApple(money);
+		//내 사과개수에 누적
 		appleCnt+=cnt;
 	}
 	public Buyer(int myMoney, int appleCnt) {
@@ -30,9 +33,12 @@ class Seller{
 	int Price;
 	//기능
 	int returnApple(int money) {
+		//보유금액에 금액 누적
 		myMoney+=money;
+		//사과개수 계산 후 보유개수에서 차감
 		int cnt=money/Price;
 		appleCnt-=cnt;
+		//사과개수 리턴
 		return cnt;
 	}
 	public Seller(int myMoney, int appleCnt, int price) {
@@ -51,9 +57,13 @@ class Seller{
 public class CH03StoreMain {
 
 	public static void main(String[] args) {
+		
 		Seller seller = new Seller(10000,100,1000);
+		
 		Buyer buyer = new Buyer(5000,0);
+		
 		buyer.Pay(seller,2000);
+		
 		System.out.println(seller);
 		System.out.println(buyer);
 
