0cb8314f34743ee25759ea90e12399509d94d53e
syrah97 <dcyc8326@naver.com>
2022-10-17 12:49:17 +0900

    SYRAH 1.0

--- src/CH11/C01ArrayBasic.java
+++ src/CH11/C01ArrayBasic.java
@@ -1,10 +1,29 @@
 package CH11;
 
+import java.util.Scanner;
+
 public class C01ArrayBasic {
 
 	public static void main(String[] args) {
 		// 길이가 5인 int형 1차원 배열의 생성
+		Scanner sc = new Scanner(System.in);
 		int[] ar1 = new int[5];
+		ar1[0]=11;
+		ar1[1]=12;
+		ar1[2]=13;
+		ar1[3]=14;
+		ar1[4]=15;
+		
+		//기존
+		for(int i=0;i<ar1.length;i++) {
+			System.out.print(ar1[i]+" ");
+		}
+		System.out.println();
+		//추가(컬렉션에도 적용가능)
+		for(int val : ar1) {
+			System.out.print(val+" ");
+		}
+		System.out.println();
 		
 		// 길이가 7일 double형 1차원 배열의 생성
 		double[] ar2 = new double[7];
---
+++ src/CH11EXEC/C01ArrayPrac.java
@@ -0,0 +1,12 @@
+package CH11EXEC;
+
+public class C01ArrayPrac {
+
+	public static void main(String[] args) {
+		// 10크기의 int형 배열을 만들어서
+		// 반복문과 함께 해당배열의 0 - 9 까지 요소에 각각 정수값을 입력
+		//합, 평균, 최대값, 최소값을 구합시다.
+
+	}
+
+}
