3d54a6c443051d87bfc638590c7763784ce438dc
dcyc8 <dcyc8@DESKTOP-EV3GGU2>
2022-10-24 12:30:14 +0900

    SYRAH 1.0

--- src/CH20/C01List.java
+++ src/CH20/C01List.java
@@ -1,9 +1,31 @@
 package CH20;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class C01List {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		List<String> list = new ArrayList();
+		
+		//추가
+		list.add("JAVA");
+		list.add("JDBC");
+		list.add("OracleDB");
+		list.add("JSP");
+		list.add("Servlet");
+		list.add("SpringFrameWork");
+		
+		
+		//확인(저장 수)
+		System.out.println("총 개체수 : " + list.size());
+		
+		//조회(요소)
+		
+		//전체 조회
+		
+		//삭제
 
 	}
 
