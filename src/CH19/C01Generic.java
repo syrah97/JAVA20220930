a608cfc83c248800ca289c23bec2bab056d4d055
dcyc8 <dcyc8@DESKTOP-EV3GGU2>
2022-10-24 10:42:05 +0900

    SYRAH 1.0

--- src/CH19/C01Generic.java
+++ src/CH19/C01Generic.java
@@ -5,6 +5,33 @@ class 팥
 	String meterial;
 	팥(String meterial){this.meterial=meterial;}
 	//toString 재정의
+	@Override
+	public String toString() {
+		return "팥 [meterial=" + meterial + "]";
+	}
+	
+}
+
+class 야채
+{
+	String meterial;
+	야채(String meterial){this.meterial=meterial;}
+	@Override
+	public String toString() {
+		return "야채 [meterial=" + meterial + "]";
+	}
+	
+}
+
+class 피자
+{
+	String meterial;
+	피자(String meterial){this.meterial=meterial;}
+	@Override
+	public String toString() {
+		return "피자 [meterial=" + meterial + "]";
+	}
+	
 }
 
 class 호빵<T>
@@ -20,8 +47,16 @@ class 호빵<T>
 
 public class C01Generic {
 	
-	private static void mian(String[] args) {
+	public static void main(String[] args) {
 		
+		호빵<팥> ob1 = new 호빵<팥>(new 팥("단팥"));
+		ob1.ShowInfo();
+		
+		호빵<야채> ob2 = new 호빵(new 야채("야채"));
+		ob2.ShowInfo();
+		
+		호빵<피자> ob3 = new 호빵(new 피자("치즈"));
+		ob3.ShowInfo();
 
 	}
 
