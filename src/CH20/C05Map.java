09a767b5047ecc1cf3d82bfaca54e8c5746d5ca1
dcyc8 <dcyc8@DESKTOP-EV3GGU2>
2022-10-24 16:04:17 +0900

    SYRAH 1.0

--- src/CH20/C05Map.java
+++ src/CH20/C05Map.java
@@ -2,6 +2,7 @@ package CH20;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class C05Map {
 
@@ -12,9 +13,19 @@ public class C05Map {
 		map.put("bbb", 2222);
 		map.put("ccc", 3333);
 		map.put("ddd", 4444);
+		map.put("ddd", 1234);	//id 중복인 경우(나중 요청된 K:V 저장)
+		map.put("fff", 1234);	//pw 중복인 경우(문제없이 저장)
 		
+		//개수
 		System.out.println("저장 수 : " + map.size());
 		
+		
+		//조회
+		Set<String> set = map.keySet();
+		for(String key : set) {
+			int value = map.get(key);
+			System.out.println("KEY : " + key + "VALUE : " + value);
+		}
 
 	}
 
