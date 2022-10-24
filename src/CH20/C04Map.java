abfb0fd1123562421ee46f3f58230a29220104fc
dcyc8 <dcyc8@DESKTOP-EV3GGU2>
2022-10-24 15:33:31 +0900

    SYRAH 1.0

--- src/CH20/C04Map.java
+++ src/CH20/C04Map.java
@@ -1,6 +1,9 @@
 package CH20;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
@@ -18,8 +21,21 @@ public class C04Map {
 		while(lotto.size()<7) {
 			lotto.add(rand.nextInt(45)+1);
 		}
+		System.out.println("----------- SET -----------");
 		for(int n : lotto) {
-			System.out.println(n+" ");
+			System.out.print(n+" ");
+		}
+		System.out.println("\n------- LIST(정렬 전) -------");
+		//정렬
+		List<Integer> list = new ArrayList(lotto);
+		for(int n : list) {
+			System.out.print(n+" ");
+		}
+		System.out.println("\n------- LIST(정렬 후) -------");
+		Collections.sort(list);
+		Collections.sort(list,Collections.reverseOrder());	//내림차순 정렬
+		for(int n : list) {
+			System.out.print(n+" ");
 		}
 			
 		
