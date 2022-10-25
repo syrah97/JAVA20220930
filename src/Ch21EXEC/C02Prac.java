a4ec4efe5b1bc53595c4c643cf9c352fe97d4522
dcyc8 <dcyc8@DESKTOP-EV3GGU2>
2022-10-25 13:28:52 +0900

    SYRAH 1.0

--- src/Ch21EXEC/C02Prac.java
+++ src/Ch21EXEC/C02Prac.java
@@ -3,8 +3,14 @@ package Ch21EXEC;
 public class C02Prac {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//메뉴를 만들어서 파일복사하는 작업을 완성해봅니다
+		//1 바이너리 파일복사
+		//2 문자형 파일 복사
+		//번호 : 1
+		//원본파일 경로 :
+		//
+		
 	}
 
 }
