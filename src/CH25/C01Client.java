060aa961027cadfec1d2e71ec7b12b9319e447af
dcyc8 <dcyc8@DESKTOP-EV3GGU2>
2022-10-27 09:42:22 +0900

    SYRAH 1.0

--- src/CH25/C01Client.java
+++ src/CH25/C01Client.java
@@ -1,9 +1,22 @@
 package CH25;
 
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.net.Socket;
+
 public class C01Client {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		Socket client = new Socket("192.168.3.124",5050);
+		
+		InputStream in = client.getInputStream();
+		DataInputStream Din = new DataInputStream(in);
+		
+		System.out.println("-----서버 메시지 START ----");
+		System.out.print(Din.readUTF()+"\n");
+		System.out.println("-----서버 메시지 END ----");
+		
+		client.close();
 
 	}
 
