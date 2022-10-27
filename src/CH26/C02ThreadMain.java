f2c7b199fc7f197f6bbfde8392c91db23b5b74f3
dcyc8 <dcyc8@DESKTOP-EV3GGU2>
2022-10-27 11:19:43 +0900

    SYRAH 1.0

--- src/CH26/C02ThreadMain.java
+++ src/CH26/C02ThreadMain.java
@@ -2,10 +2,25 @@ package CH26;
 
 public class C02ThreadMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
-		//메인스레드의 실행흐름
+		//작업스레드 객체 생성
+		C02Worker1 worker1 = new C02Worker1();
+		C02Worker2 worker2 = new C02Worker2();
 		
+		//독립된 스레드 공간에 작업스레드 전달
+		Thread th1 = new Thread(worker1);
+		Thread th2 = new Thread(worker2);
+		
+		//스레드 실행
+		th1.start();
+		th2.start();
+		
+		//메인스레드에서의 실행흐름
+		for(int i=0;i<5;i++) {
+			System.out.println("MAINThread");
+			Thread.sleep(500);
+		}
 
 	}
 
