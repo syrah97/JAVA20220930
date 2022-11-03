38f84da8d06a82578ed913bb8e78c38174d5d3ad
dcyc8 <dcyc8@DESKTOP-EV3GGU2>
2022-11-03 15:19:29 +0900

    SYRAH 1.0

--- src/CH32/C03DBUpdate.java
+++ src/CH32/C03DBUpdate.java
@@ -9,22 +9,18 @@ public class C03DBUpdate {
 
 	public static void main(String[] args) {
 		//연결관련 정보 저장용 변수
-		String id = "root";	//DB연결 id
+		String id = "SYSTEM";	//DB연결 id
 		String pw = "1234";	//DB연결 pw
-		String url = "jdbc:mysql://localhost:3306/shopdb";	//연결URL(DBMS마다 상이함)
-		
-		//DB연결객체 관련 참조변수
-		Connection conn = null;				//DB연결객체용 참조변수
-		PreparedStatement pstmt = null;		//SQL쿼리 전송객체용 참조변수
-		ResultSet rs =null;					//쿼리결과(Select결과)수신용 참조변수
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";	//연결URL(DBMS마다 상이함)
 		
 		//연결하기
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");	//DB 드라이버 로드
-			System.out.println("Driver Loading Success!!");
-			conn = DriverManager.getConnection(url,id,pw);	//DB Connection 객체받기
-			System.out.println("DB Connected...");
-			pstmt=conn.prepareStatement("update tbl_customer set addr=? where id=?");
+		try 
+		(
+				Connection conn = DriverManager.getConnection(url,id,pw);	//DB Connection 객체받기
+				PreparedStatement pstmt=conn.prepareStatement("");
+		)
+		
+		{			
 			pstmt.setString(1, "마산");
 			pstmt.setInt(2, 3);
 			
@@ -36,9 +32,6 @@ public class C03DBUpdate {
 			}
 		}catch(Exception e) {
 			e.printStackTrace();
-		}finally {
-			try {pstmt.close();}catch(Exception e) {e.printStackTrace();}
-			try {conn.close();}catch(Exception e) {e.printStackTrace();}
 		}
 
 	}
